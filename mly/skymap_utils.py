cfa8b5ef5d072765f6a6bf3828b56dbd53dd2886
VASILEIOS SKLIRIS <vasileios.skliris@ligo.org>
2024-03-21 14:25:16 -0700

    fix

--- mly/skymap_utils.py
+++ mly/skymap_utils.py
@@ -253,12 +253,15 @@ def mask_window_tf(T, fs, start_time, end_time, ramp_duration,ramp_centre, durat
     # print(len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad))
 
     # Adjusting the length when sometimes a pixel or two missing due to the use of int and ceil.
-    if tf.cast(len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad),tf.float32) < tf.cast(T*fs*1.0,tf.float32):
+    
+    thesum = tf.cast(len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad),tf.float32)
+
+    if thesum < T*fs:
         diff = tf.cast( T*fs -  len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad) , tf.int32 ) 
 
         centre = tf.concat([centre, tf.ones(diff)],0)
     
-    elif tf.cast(len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad),tf.float32) > tf.cast(T*fs*1.0,tf.float32):
+    elif thesum > T*fs:
 
         diff = tf.cast( len(left_pad)+len(left_ramp)+len(centre)+len(right_ramp)+len(right_pad) - T*fs , tf.int32 )
 
