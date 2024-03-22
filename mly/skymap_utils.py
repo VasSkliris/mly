b45078327491e4803f7550702b646a3c95e25053
VASILEIOS SKLIRIS <vasileios.skliris@ligo.org>
2024-03-22 03:27:30 -0700

    notched strain before

--- mly/skymap_utils.py
+++ mly/skymap_utils.py
@@ -839,10 +839,12 @@ def skymap_gen_function(strain,fs, uwstrain, psd, gps, detectors,PE
     
     notched_strain = remove_line(uwstrain, fs, f_min=window_parameter[-2], f_max=window_parameter[-1], Q=30.0, factor=10)
 
+    notched_strain_before = notched_strain.whiten(4,2,fduration=4,method = 'welch'
+                                 , highpass=20).value
     w = int(notched_strain.shape[1]/fs)
 
     notched_strain= notched_strain[:,int(((w-1)/2)*fs):int(((w+1)/2)*fs)]
-
+    notched_strain_before[:,int(((w-1)/2)*fs):int(((w+1)/2)*fs)]
     start = time.time()
     sky_map = EnergySkyMaps(notched_strain,
                             time_delay_map, 
