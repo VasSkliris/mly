d73519dc9af403fb39a4e097b077c4124b83f8ee
VASILEIOS SKLIRIS <vasileios.skliris@ligo.org>
2024-03-22 03:11:57 -0700

    typo

--- mly/skymap_utils.py
+++ mly/skymap_utils.py
@@ -574,8 +574,8 @@ def EnergySkyMaps(
 def bandpass(data, fs, f_min, f_max, filter_order=10):
     
     from gwpy.timeseries import TimeSeries
-
-    bandpassed_data = list( TimeSeries(data[i],sample_rate = fs).bandpass(fmin, fmax) for i in range(len(data)) )
+    print('f',fmin,fmax)
+    bandpassed_data = list( TimeSeries(data[i],sample_rate = fs).bandpass(flow = fmin, fhigh = fmax) for i in range(len(data)) )
 
     # # ---- Construct bandpass filter.
     # b, a = butter(filter_order, [f_min, f_max], btype='bandpass', output='ba', fs=fs)
